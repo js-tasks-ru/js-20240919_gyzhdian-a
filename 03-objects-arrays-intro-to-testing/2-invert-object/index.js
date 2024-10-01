727b4b59996322e0ea61b25417e0350e15549c5b
alena.gyzhdiyan <alena@MacBook-Pro-Alena.local>
2024-10-01 23:17:02 +0300

    create array with uniq items

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,8 +4,8 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-  if(!obj){return undefined}
-    const entries = Object.entries(obj);
+  if (!obj) {return undefined;}
+  const entries = Object.entries(obj);
   let result = [];
   for (const [key, value] of entries) {
     result.push([value, key]);
