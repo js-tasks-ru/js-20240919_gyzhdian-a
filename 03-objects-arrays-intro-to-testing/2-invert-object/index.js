6ce8a40a023bec99e0db96870373497a99d2e8fd
alena.gyzhdiyan <alena@MacBook-Pro-Alena.local>
2024-10-01 23:07:01 +0300

    create invert object function

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,12 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if(!obj){return undefined}
+    const entries = Object.entries(obj);
+  let result = [];
+  for (const [key, value] of entries) {
+    result.push([value, key]);
+  }
+  return Object.fromEntries(result);
 
 }
